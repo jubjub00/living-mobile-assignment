b8d94dd2c02b02e8ffe8c368825f9aca83831db9
chukeit panpiboon <chukeit@livingmobile.me>
2021-06-18 12:14:55 +0700

    update, delete -> menu

--- backend-rest/src/menu/menu.controller.ts
+++ backend-rest/src/menu/menu.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { MenuService } from './menu.service';
 import { CreateMenuDto } from './dto/createMenu.dto';
 import { ApiOperation } from '@nestjs/swagger';
@@ -16,5 +16,23 @@ export class MenuController {
     @ApiOperation({ summary: 'Find all menu' })
     findAll() {
         return this.menuService.findAll();
+    }
+
+    @Get(':name')
+    @ApiOperation({ summary: 'Search menu' })
+    findOne(@Param('name') name: string) {
+        return this.menuService.findOne(name);
+    }
+
+    @Delete(':id')
+    @ApiOperation({ summary: 'Delete menu' })
+    remove(@Param('id') id: string) {
+        return this.menuService.remove(id);
+    }
+
+    @Put(':id')
+    @ApiOperation({ summary: 'Update menu' })
+    update(@Param('id') id: string, @Body() updateStoreDto: CreateMenuDto) {
+        return this.menuService.update(id, updateStoreDto);
     }
 }
